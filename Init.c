dc015a61657f119cf459f6ab823e36c06536321c
ditoaugusta <dt_ap@ymail.com>
2015-11-25 23:40:38 +0700

    Driver Servo
    Fungsi Servo ditambahkan

--- Init.c
+++ Init.c
@@ -8,6 +8,103 @@
 #include "DrvGPIO.h"
 #include "DrvSPI.h"
 #include "DrvADC.h"
+#include "DrvPWM.h"
+#include "DrvSYS.h"
+
+/*************************************************************
+ * GPIOA Initialization
+**************************************************************/
+void GPIOA_Init()
+{
+   //
+   // Set PA0 Pin Mode
+   //
+   DrvGPIO_Open(E_GPA, 0, E_IO_QUASI);
+   //
+   // Set PA1 Pin Mode
+   //
+   DrvGPIO_Open(E_GPA, 1, E_IO_QUASI);
+   //
+   // Set PA2 Pin Mode
+   //
+   DrvGPIO_Open(E_GPA, 2, E_IO_QUASI);
+   //
+   // Set PA3 Pin Mode
+   //
+   DrvGPIO_Open(E_GPA, 3, E_IO_QUASI);
+   //
+   // Set PA4 Pin Mode
+   //
+   DrvGPIO_Open(E_GPA, 4, E_IO_QUASI);
+   //
+   // Set PA5 Pin Mode
+   //
+   DrvGPIO_Open(E_GPA, 5, E_IO_QUASI);
+
+}
+
+/*************************************************************
+* PWM Initialization Code Template
+**************************************************************/
+void PWM_Init()
+{
+    S_DRVPWM_TIME_DATA_T sPt_0;
+    uint8_t u8Timer_0;
+    S_DRVPWM_TIME_DATA_T sPt_1;
+    uint8_t u8Timer_1;
+    /* Enable PWM clock */
+    DrvPWM_Open();
+
+    /* Set PWM0 Pin */
+    outpw(&SYS->GPAMFP, inpw(&SYS->GPAMFP) | (0x1<<(12)));
+
+    /* Selcect pwm0 clock source */
+    DrvPWM_SelectClockSource(DRVPWM_TIMER0, DRVPWM_EXT_12M);
+
+    /* PWM Timer property */
+    sPt_0.u8Mode = DRVPWM_ONE_SHOT_MODE;
+    sPt_0.u32Frequency = 0;
+    sPt_0.u8PreScale = 0;
+    sPt_0.u8ClockSelector = DRVPWM_CLOCK_DIV_1;
+    sPt_0.u8HighPulseRatio = 0;
+    sPt_0.i32Inverter = FALSE;
+    sPt_0.u32Duty = 0;
+    u8Timer_0 = DRVPWM_TIMER0;
+
+    /* PWM Timer0 Configuration */
+    DrvPWM_SetTimerClk(DRVPWM_TIMER0, &sPt_0);
+
+    /* Enable in/out for PWM Timer0 */
+    DrvPWM_SetTimerIO(DRVPWM_TIMER0, 1);
+
+    /* Enable the PWM Timer 0 */
+    DrvPWM_Enable(DRVPWM_TIMER0, 1);
+
+    /* Set PWM1 Pin */
+    outpw(&SYS->GPAMFP, inpw(&SYS->GPAMFP) | (0x1<<13));
+
+    /* Enable PWM clock */
+    DrvPWM_SelectClockSource(DRVPWM_TIMER1, DRVPWM_EXT_12M);
+
+    /* PWM1 Timer property */
+    sPt_1.u8Mode = DRVPWM_ONE_SHOT_MODE;
+    sPt_1.u32Frequency = 0;
+    sPt_1.u8PreScale = 0;
+    sPt_1.u8ClockSelector = DRVPWM_CLOCK_DIV_1;
+    sPt_1.u8HighPulseRatio = 0;
+    sPt_1.i32Inverter = FALSE;
+    sPt_1.u32Duty = 0;
+    u8Timer_1 = DRVPWM_TIMER1;
+
+    /* Set PWM Timer1 Configuration */
+    DrvPWM_SetTimerClk(DRVPWM_TIMER1, &sPt_1);
+
+    /* Enable in/out for PWM Timer1 */
+    DrvPWM_SetTimerIO(DRVPWM_TIMER1, 1);
+
+    /* Enable the PWM Timer 1 */
+    DrvPWM_Enable(DRVPWM_TIMER1, 1);
+}
 
 /*************************************************************
  * GPIOD Initialization
@@ -65,4 +162,6 @@ void Init()
     GPIOD_Init();
     SPI3_Init();
     ADC_Init();
+    GPIOA_Init();
+    PWM_Init();
 }
--- main.c
+++ main.c
@@ -2,24 +2,135 @@
 #include "DrvSYS.h"
 #include "LCD_Driver.h"
 #include "DrvADC.h"
+#include "PWM.h"
+
+//#define  SERVO_CYCTIME        2000 // 20ms = 50Hz
+#define  SERVO_HITIME_MIN       50 // minimum Hi width = 0.5ms
+#define  SERVO_HITIME_MAX      250 // maximum Hi width = 2.5ms
+#define  STEPTIME               10 // incremental time = 1.0ms
+
 void Init();
+
+
+	//---------------------------
+
+void read_ldr(int x) {
+
+	int nilai;
+	nilai = x;
+	clr_all_pannal();
+	print_lcd(0,"LDR = ");
+
+	Show_Word(0,7,nilai/1000+'0');
+	Show_Word(0,8,nilai%1000/100+'0');
+	Show_Word(0,9,nilai%100/10+'0');
+	Show_Word(0,10,nilai%10+'0');
+	DrvSYS_Delay(100000);
+}
+
 int main(void)
 {
-	int nilai;
     Init();
+    //inisialisasi variabel PWM
+    	//uint8_t i;
+        uint8_t hitime;
+    	//char TEXT0[16],TEXT1[16],TEXT2[16],TEXT3[16];
+    	uint32_t Clock;
+    	uint32_t Frequency;
+    	uint8_t  PreScaler;
+    	uint8_t  ClockDivider;
+    	uint8_t  DutyCycle;
+    	uint16_t CNR, CMR;
+    	//-------------------------
+    	void servo_buka(){
+    			//Assignment variabel PWM
+    			UNLOCKREG();
+    			SYSCLK->PWRCON.XTL12M_EN=1;
+    			DrvSYS_Delay(5000);					// Waiting for 12M Xtal stalble
+    			SYSCLK->CLKSEL0.HCLK_S=0;
+    			LOCKREG();
+
+    			//init_LCD();
+    			//clear_LCD();
+
+    			// PWM_No = PWM channel number
+    			// PWM_CLKSRC_SEL   = 0: 12M, 1:32K, 2:HCLK, 3:22M
+    			// PWM_PreScaler    : PWM clock is divided by (PreScaler + 1)
+    			// PWM_ClockDivider = 0: 1/2, 1: 1/4, 2: 1/8, 3: 1/16, 4: 1
+    			init_PWM(0, 0, 130, 4); // initialize PWM0(GPA12) to output 1MHz square wave
+    			Clock = 12000000;
+    			PreScaler = 130;
+    			ClockDivider = 1;
+    			Frequency = 50;
+
+    			//hitime = SERVO_HITIME_MIN;
+    			//--------------------------------------
+
+    			//----------Kodingan inti microservo
+    			//hitime=hitime + (SERVO_HITIME_MAX - SERVO_HITIME_MIN)/10;
+    			hitime = 125;
+    			if (hitime>SERVO_HITIME_MAX) hitime = SERVO_HITIME_MIN;
+    			//PWM_FreqOut = PWM_Clock / (PWM_PreScaler + 1) / PWM_ClockDivider / (PWM_CNR + 1)
+    			CNR = Clock / Frequency / (PreScaler + 1) / ClockDivider - 1;
+    			// Duty Cycle = (CMR0+1) / (CNR0+1)
+    			CMR = hitime  - 1;
+    			//CMR = 20;
+    			PWM_Out(0, CNR, CMR);
+    		}
+    	void servo_tutup(){
+    			//Assignment variabel PWM
+    			UNLOCKREG();
+    			SYSCLK->PWRCON.XTL12M_EN=1;
+    			DrvSYS_Delay(5000);					// Waiting for 12M Xtal stalble
+    			SYSCLK->CLKSEL0.HCLK_S=0;
+    			LOCKREG();
+
+    			//init_LCD();
+    			//clear_LCD();
+
+    			// PWM_No = PWM channel number
+    			// PWM_CLKSRC_SEL   = 0: 12M, 1:32K, 2:HCLK, 3:22M
+    			// PWM_PreScaler    : PWM clock is divided by (PreScaler + 1)
+    			// PWM_ClockDivider = 0: 1/2, 1: 1/4, 2: 1/8, 3: 1/16, 4: 1
+    			init_PWM(0, 0, 60, 4); // initialize PWM0(GPA12) to output 1MHz square wave
+    			Clock = 12000000;
+    			PreScaler = 60;
+    			ClockDivider = 1;
+    			Frequency = 50;
+
+    		//	hitime = SERVO_HITIME_MIN;
+
+    			//----------Kodingan inti microservo
+    			//hitime=hitime + (SERVO_HITIME_MAX - SERVO_HITIME_MIN)/10;
+    			hitime = 125;
+    			if (hitime>SERVO_HITIME_MAX) hitime = SERVO_HITIME_MIN;
+    			//PWM_FreqOut = PWM_Clock / (PWM_PreScaler + 1) / PWM_ClockDivider / (PWM_CNR + 1)
+    			CNR = Clock / Frequency / (PreScaler + 1) / ClockDivider - 1;
+    			// Duty Cycle = (CMR0+1) / (CNR0+1)
+    			CMR = hitime  - 1;
+    			//CMR = 20;
+    			PWM_Out(0, CNR, CMR);
+    		}
+
+    int adc,state;
     Initial_pannel();
     DrvGPIO_ClrBit(E_GPD,14);
+    state = 1;
     while(1)
     {
-    	clr_all_pannal();
-    	print_lcd(0,"LDR = ");
     	DrvADC_StartConvert();
-    	nilai=DrvADC_GetConversionData(0);
-
-    	Show_Word(0,7,nilai/1000+'0');
-    	Show_Word(0,8,nilai%1000/100+'0');
-    	Show_Word(0,9,nilai%100/10+'0');
-    	Show_Word(0,10,nilai%10+'0');
-    	DrvSYS_Delay(100000);
+    	adc=DrvADC_GetConversionData(0);
+    	read_ldr(adc);
+    	DrvSYS_Delay(1000000);
+    	if (adc>3000) {
+    		state = 0;
+    		servo_tutup();
+    	} else if (adc<900){
+    		state = 0;
+    		servo_tutup();
+    	} else {
+    		state = 1;
+    		servo_buka();
+    	}
     }
 }
